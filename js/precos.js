ea769afe17a060000f31fa0be1de461a41f948f3
nicole <nicoleguimaraesqa@gmail.com>
2026-08-16 17:42:55 -0300

    deploy: atualizar preco do Adizero Evo SL

--- js/precos.js
+++ js/precos.js
@@ -47,7 +47,8 @@
     }
 
     if (produto.categoria === 'Tênis') {
-      produto.preco = 649.90;
+      if (/adidas\s+Adizero\s+Evo\s+SL/i.test(produto.nome)) produto.preco = 589.90;
+      else produto.preco = 649.90;
       return;
     }
 
